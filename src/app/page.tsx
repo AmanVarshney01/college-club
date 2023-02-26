ed24ccb91e78464aec91b9a9372b8d7c74469039
Aman Varshney <av.amanvarshney11@gmail.com>
2023-02-26 13:47:43 +0530

    style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
             <h1 className={"text-[20rem] text-[#2f8d46] drop-shadow-lg"}><span
                 className={"bg-gradient-to-tr from-[#2f8d46] text-transparent bg-clip-text to-[#F7F7F9] animate-text"}>GFG GLAU</span>
             </h1>
-            {/*<h1 className={"bg-[#353A47]"}></h1>*/}
         </main>
     )
 }
