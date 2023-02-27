e453f8e20e8d4f8900b0de8fa1ecff2092c9a469
Aman Varshney <av.amanvarshney11@gmail.com>
2023-02-27 22:14:24 +0530

    optimizations

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,10 +1,12 @@
 import Button from "@/app/button";
+import Image from "next/image";
+import glaLogo from 'public/gla logo A+.png';
 
 export default function Navbar() {
     return (
             <nav className={""}>
                 <div className={"fixed top-0 left-0 m-10"}>
-                    <img className={"w-full h-28"} src="/gla%20logo%20A+.png" alt="gla university"/>
+                    <Image className={"h-28 w-full"} placeholder='blur' src={glaLogo} alt="gla university"/>
                 </div>
                 <div className={"fixed bottom-0 left-0 right-0 w-screen mb-10"}>
                     <ul className={"flex justify-center gap-8 text-white text-2xl"}>
