33967ba68ab45e01340b08e8d8d1824b52e45de0
Aman Varshney <av.amanvarshney11@gmail.com>
2023-02-28 00:14:21 +0530

    a little responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             className={'w-screen h-screen flex justify-center items-center bg-[#131200]'}>
             <div className={"relative"}>
                 <h1 className={"text-[#2f8d46] drop-shadow-lg"}>
-                <span className={`lg:text-[20rem] text-[6rem] bg-gradient-to-br from-[#2f8d46] text-transparent bg-clip-text to-[#78BC61] animate-text via-[#73be73] ${lilitaOne.variable} font-sans`}>
+                <span className={`2xl:text-[20rem] xl:text-[18rem] sm:text-[8rem] text-[6rem] lg:text-[15rem] md:text-[11rem] bg-gradient-to-br from-[#2f8d46] text-transparent bg-clip-text to-[#78BC61] animate-text via-[#73be73] ${lilitaOne.variable} font-sans`}>
                     GFG GLAU
                 </span>
                 </h1>
