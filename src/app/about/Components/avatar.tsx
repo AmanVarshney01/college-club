3830ad8e73f86b2b9ec65533053b0d9da1e7fe82
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-06 20:48:00 +0530

    added  metadata

--- src/app/about/Components/avatar.tsx
+++ src/app/about/Components/avatar.tsx
@@ -3,7 +3,7 @@ export default function Avatar ({name, post, photo}: {name: string, post: string
     return (
         <div className={"w-64 h-96 flex-shrink-0 bg-white relative group"}>
             <div className={"opacity-0 hover:opacity-100 transition duration-700"}>
-                <div className={"absolute bottom-0 w-full h-full opacity-50  bg-black"}></div>
+                <div className={"absolute bottom-0 w-full h-full opacity-70 blur-sm bg-black"}></div>
                 <div className={"absolute bottom-0 w-full h-full grid place-content-center"}>
                     <div className={"flex flex-col place-items-center w-full "}>
                         <span className={"text-2xl"}>{name}</span>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,47 @@
 import './globals.css'
-import { AnalyticsWrapper } from './analytics';
+import {AnalyticsWrapper} from './analytics';
 // import Navbar from "@/app/navbar";
 // import Alert from "@/app/Components/alert";
 
 
+export const metadata = {
+    title: 'GFG GLAU',
+    description: 'Official Website of GFG GLAU',
+    creator: 'Aman Varshney',
+    keywords: ['geeksforgeeks', 'gla university', 'gfg glau', "geeksforgeeks student chapter gla university"],
+    // url: 'https://gfg-glau.vercel.app/',
+    icons: {
+        icon: '/gfgglau.png'
+    },
+    category: 'technology',
+    appLinks: {
+        web: {
+            url: 'https://gfg-glau.vercel.app/',
+        }
+    }
+}
+
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head ><title>GFG GLAU</title></head>
-      <body className={"relative"}>
-      {/*<Alert  text={"Register Now! Entrevista: Mock placement Drive"}/>*/}
-      {children}
-      <AnalyticsWrapper />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"/>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap"
+                  rel="stylesheet"/>
+        </head>
+        <body className={"relative"}>
+        {/*<Alert  text={"Register Now! Entrevista: Mock placement Drive"}/>*/}
+        {children}
+        <AnalyticsWrapper/>
+        </body>
+        </html>
+    )
 }
