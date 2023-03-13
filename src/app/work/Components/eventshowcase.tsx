8db280bcc10a3f67947d2d2d3f69f6dd44c8e0ae
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-14 02:25:54 +0530

    styling changes

--- src/app/work/Components/eventshowcase.tsx
+++ src/app/work/Components/eventshowcase.tsx
@@ -2,16 +2,17 @@ export default function EventShowcase({title, subTitle, description}: {title: st
     return (
         <div className={"w-screen h-screen flex flex-row flex-shrink-0 bg-transparent"}>
             <div className={"flex flex-col place-items-center basis-2/5"}>
-                <div className={"h-1/2 grid place-content-center gap-6"}>
-                    <div className={"flex flex-col gap-6 text-left place-items-center"}>
-                        <h2 className={"text-6xl"}>{title}</h2>
+                <div className={"h-1/2 flex items-end justify-center"}>
+                    <div className={"flex flex-col text-left place-items-center"}>
+                        <h2 className={"text-6xl mb-3.5"}>{title}</h2>
                         <h3 className={"text-xl text-gray-200"}>{subTitle}</h3>
                     </div>
                 </div>
-                <div className={"h-min"}><p>{description}</p></div>
-                <div className={"h-full w-full"}></div>
+                <div className={"h-full w-full flex items-center justify-center"}>
+                    <div className={""}><p>{description}</p></div>
+                </div>
                 <div className={"w-full h-max flex justify-center"}>
-                    <div className={"flex flex-row text-xl px-6 py-10 border my-12"}>
+                    <div className={"flex flex-row text-xl px-6 py-10 border my-20"}>
                         <span className={"mx-3.5"}>Number of Event Participants:</span>
                         <span className={""}>7000+</span>
                     </div>
