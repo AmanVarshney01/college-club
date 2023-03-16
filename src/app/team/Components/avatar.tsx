fe71bb9cb91966779b941b1fd1f206e24fe359f8
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-16 23:17:40 +0530

    added icons in teams avatar

--- src/app/team/Components/avatar.tsx
+++ src/app/team/Components/avatar.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
+import instagram from 'public/socialIcons/instagram.png'
+import whatsapp from 'public/socialIcons/whatsapp.png'
+import linkedin from 'public/socialIcons/linkedin.png'
 export default function Avatar ({name, post, photo}: {name: string, post: string, photo: any}) {
     return (
         <div className={"w-64 flex-shrink-0 bg-white relative group"}>
             <Image className={"bg-cover"} src={photo} alt={name} />
-            <div className={"w-64 h-32 bg-[#181818]"}>
+            <div className={"h-36 w-64 bg-[#181818]"}>
                 <div className={""}>
                     {/*<div className={"absolute bottom-0 w-full opacity-70 blur-sm bg-black"}></div>*/}
-                    <div className={"w-full flex flex-col justify-center items-center p-3"}>
-                        <div className={"flex flex-col place-items-center w-full"}>
-                            <span className={"text-2xl"}>{name}</span>
+                    <div className={"w-full flex flex-col justify-center items-center p-3 h-max"}>
+                        <div className={"flex flex-col justify-evenly items-center w-full gap-2"}>
+                            <span className={"text-xl font-semibold"}>{name}</span>
                             <span>{post}</span>
+                            <div className={"flex flex-row justify-evenly w-full px-6 border-t pt-4"}>
+                                <a href=""><Image width={25} height={25} src={instagram} alt={"instagram"} /></a>
+                                <a href=""><Image width={25} height={25} src={linkedin} alt={"linkedin"} /></a>
+                                <a href=""><Image width={25} height={25} src={whatsapp} alt={"whatsapp"} /></a>
+                            </div>
                         </div>
                     </div>
                 </div>
