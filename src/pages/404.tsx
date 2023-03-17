0334369df4bdcb6d9d18ef62ac2992d9deaa3dc6
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-17 22:36:09 +0530

    custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,26 @@
+import mrincredible from 'public/cute_mr_incredible.png'
+import Image from "next/image";
+import "./errorpage.css"
+
+export default function Custom404() {
+
+    return (<div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100vh',
+            width: "100%",
+            gap: '2rem',
+            backgroundColor: '#F2F2F2',
+            margin: '0px',
+            padding: '0px',
+        }}>
+            <Image style={{
+                width: 'auto', height: '35%',
+            }} src={mrincredible} alt={"cute mr.incredible"}/>
+
+            <h1 style={{
+                fontSize: '3rem', fontFamily: 'monospace',
+            }}>404 - Page Not Found</h1>
+        </div>);
+};
