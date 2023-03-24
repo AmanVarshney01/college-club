6794ce2bd5735ca0e1f515775aebe19e46ba53de
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-25 02:01:29 +0530

    styling changes

--- src/app/home/HomePage.tsx
+++ src/app/home/HomePage.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/app/home/Components/navbar";
 export default function HomePage () {
 
     return (
-        <section id="homepage" className={" min-h-screen flex flex-col justify-evenly items-center bg-transparent 2xl:px-20 px-4 "}>
+        <section id="homepage" className={" min-h-screen flex flex-col justify-around items-center bg-transparent 2xl:px-20 px-4 "}>
             <Bgicons/>
             <Navbar/>
             <div className={"items-center justify-start h-full w-full flex"}>
