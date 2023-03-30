826a3f47fc84fd826faa3b92e27df5d5a7ff1147
Aman Varshney <av.amanvarshney11@gmail.com>
2023-03-30 21:11:20 +0530

    styling changes

--- src/app/home/Components/maintitle.tsx
+++ src/app/home/Components/maintitle.tsx
@@ -3,8 +3,8 @@ import gfg from 'public/gfg_logo.png'
 
 export default function MainTitle() {
     return (
-        <div className={"flex z-20 flex-col relative w-full px-[6vw]"}>
-            <Image className={""} width={600} src={gfg} alt={"gfg"}/>
+        <div className={"flex z-20 flex items-center justify-end relative w-full basis-2/5"}>
+            <Image className={""} width={400} src={gfg} alt={"gfg"}/>
             {/*<h1 className={" drop-shadow-lg w-max h-max"}>*/}
             {/*    <span*/}
             {/*        id="title"*/}
--- src/app/home/HomeSection.tsx
+++ src/app/home/HomeSection.tsx
@@ -8,7 +8,7 @@ import gla from 'public/gla3.png'
 export default function HomeSection () {
 
     return (
-        <section id="homepage" className={"min-h-[100svh] w-full relative flex justify-center items-center bg-transparent lg:px-4 lg:py-2 px-2 py-1"}>
+        <section id="homepage" className={"min-h-[100svh] w-full relative flex justify-center items-center flex-row bg-transparent lg:px-4 lg:py-2 px-2 py-1"}>
             {/*<Bgicons/>*/}
             <MainTitle />
             <div className={"absolute -bottom-10 lg:-right-24 lg:w-7/12 w-11/12 h-auto"}>
@@ -17,6 +17,7 @@ export default function HomeSection () {
                     <Image className={"pointer-events-none opacity-60"} src={gla} alt={"gla"} />
                 </div>
             </div>
+            <div className={"basis-3/5"}></div>
             {/*<Image className={"fixed bottom-0 right-0 w-1/2 opacity-90"} src={gla} alt={"gla"} />*/}
         </section>
     )
