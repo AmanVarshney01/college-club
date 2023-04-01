1d9f71f88307e68974eea7e6f1f5d30ac16d471a
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-01 16:59:11 +0530

    removed all references of gfg glau

--- src/app/home/Components/navbar.tsx
+++ src/app/home/Components/navbar.tsx
@@ -1,8 +1,7 @@
 "use client";
 import NavButton from "@/app/home/Components/NavButton";
-import glaGlauLogo from 'public/gfg.png';
-import Image from "next/image";
-import {motion as m} from "framer-motion";
+// import Image from "next/image";
+// import {motion as m} from "framer-motion";
 
 export default function Navbar() {
 
@@ -35,7 +34,7 @@ export default function Navbar() {
         <div className={"bg-gradient-to-b from-[#121212]/100 to-[#121212]/0 fixed left-0 top-0 w-full h-min z-50"}>
             <div className={"flex flex-row justify-between items-center gap-16 w-full py-1 px-4 lg:py-2 lg:px-6"}>
                 <div className={" z-10 flex flex-row gap-5 items-center w-fit"}>
-                    <m.a initial={{scale: 0, opacity: 0}} animate={{opacity: 1, scale: 1}} href="/" className={""}><Image placeholder="blur" width={250} id="linkCursor" src={glaGlauLogo} alt="gla glau "/></m.a>
+                    {/*<m.a initial={{scale: 0, opacity: 0}} animate={{opacity: 1, scale: 1}} href="/" className={""}><Image placeholder="blur" width={250} id="linkCursor" src={null} alt=""/></m.a>*/}
                 </div>
                 <nav className={"flex 2xl:gap-8 gap-3 w-fit relative"}>
                     <NavButton href={"/about"}  name={"About"}/>
