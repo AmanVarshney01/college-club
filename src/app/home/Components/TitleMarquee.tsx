5c612843fb23d69763b7eb0e60b54ceaa93c8f37
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-03 02:45:09 +0530

    added a infinite text marquee in home page

---
+++ src/app/home/Components/TitleMarquee.tsx
@@ -0,0 +1,11 @@
+
+export default function TitleMarquee() {
+    return (
+       <div id={"marquee-container"} className={" flex justify-start overflow-hidden tracking-widest text-2xl"}>
+           <h2 className={"whitespace-nowrap px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
+           <h2 className={"whitespace-nowrap px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
+           <h2 className={"whitespace-nowrap px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
+           <h2 className={"whitespace-nowrap px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
+       </div>
+    )
+}
--- src/app/home/HomeSection.tsx
+++ src/app/home/HomeSection.tsx
@@ -1,4 +1,5 @@
 import MainTitle from "@/app/home/Components/maintitle";
+import TitleMarquee from "@/app/home/Components/TitleMarquee";
 // import Image from "next/image";
 // import gla from 'public/gla3.png'
 // import Image from "next/image";
@@ -7,8 +8,10 @@ import MainTitle from "@/app/home/Components/maintitle";
 export default function HomeSection () {
 
     return (
-        <section id="homepage" className={"min-h-[100svh] z-10 w-full relative flex justify-center items-center flex-row bg-transparent lg:px-4 lg:py-2 px-2 py-1"}>
-            <MainTitle />
+        <section id="homepage" className={"min-h-[100svh] z-10 w-full flex justify-center items-center flex-col relative bg-transparent lg:px-4 lg:py-2 px-2 py-1"}>
+            <div className={""}>
+                <MainTitle />
+            </div>
             {/*<div className={"absolute -bottom-10 lg:-right-24 lg:w-7/12 w-11/12 h-auto"}>*/}
             {/*    <div className={"relative"}>*/}
             {/*        <div className={"w-full h-full absolute from-10% to-90% bg-gradient-to-t from-[#121212]/100 to-[#121212]/0 z-10"}></div>*/}
@@ -16,6 +19,9 @@ export default function HomeSection () {
             {/*    </div>*/}
             {/*</div>*/}
             {/*<Image className={"fixed bottom-0 right-0 w-1/2 opacity-90"} src={gla} alt={"gla"} />*/}
+            <div className={"absolute bottom-0 left-0"}>
+                <TitleMarquee />
+            </div>
         </section>
     )
 }
