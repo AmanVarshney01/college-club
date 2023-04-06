c542762fd3b374d8b79032d7dbebed435fd060c6
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-06 16:16:40 +0530

    removed dynamic import compressed university photo

--- src/app/home/MissionSection.tsx
+++ src/app/home/MissionSection.tsx
@@ -8,12 +8,12 @@ export default function MissionSection () {
                 <h2 className={"text-2xl"}>Our Mission</h2>
                 <span className={"text-2xl"}>is to cultivate a vibrant</span>
                 <span className={"text-7xl font-semibold"}>Coding Culture</span>
-                <span className={"text-4xl"}>on Campus</span>
+                <span className={"text-2xl"}>on Campus</span>
                 {/*<span className={""}>Through our various programs, events, and initiatives, we strive to create a welcoming and inclusive community of coders, where members can learn, share ideas, and collaborate on projects.</span>*/}
             </div>
             {/*our mission is to cultivate a vibrant coding culture on campus*/}
             <div className={"w-1/2 h-full z-0"}>
-                <Image className={""} src={university} alt={"University"}/>
+                <Image loading={"lazy"} placeholder={"blur"} className={""} src={university} alt={"University"}/>
             </div>
 
         </section>
