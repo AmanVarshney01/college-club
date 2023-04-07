1642dbbc88602862c0930485b9123e0e66f7f657
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-07 17:33:46 +0530

    plz work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 import HomeSection from "@/app/home/HomeSection";
 import MissionSection from "@/app/home/MissionSection";
-// import dynamic from "next/dynamic";
 import {PageWrapper} from "@/app/page-wrapper";
 import EventSection from "@/app/home/EventSection";
-
-// const MissionSection = dynamic(() => import("@/app/home/MissionSection"));
 
 export default function Home() {
 
     return (
             <PageWrapper>
                 <main id="container" className={"z-10"}>
-                    <HomeSection/>
+                    {/*<HomeSection/>*/}
                     <MissionSection />
                     <EventSection />
                 </main>
