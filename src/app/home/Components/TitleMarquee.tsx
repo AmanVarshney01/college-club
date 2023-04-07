fa095e16db10849b8e9bd2e1b99a667c0e78390d
Aman Varshney <av.amanvarshney11@gmail.com>
2023-04-07 19:28:53 +0530

    Revert "changed styling in homepage"
    This reverts commit 3734daad5d6191b738792a1eaef066fa809545a9.

--- src/app/home/Components/TitleMarquee.tsx
+++ src/app/home/Components/TitleMarquee.tsx
@@ -1,7 +1,7 @@
 
 export default function TitleMarquee() {
     return (
-       <div id={"marquee-container"} className={"w-[100svw] flex justify-start overflow-hidden tracking-widest text-2xl uppercase whitespace-nowrap hover:text-[#D8E9A8]"}>
+       <div id={"marquee-container"} className={"flex justify-start overflow-hidden tracking-widest text-2xl uppercase whitespace-nowrap hover:text-[#D8E9A8]"}>
            <h2 className={"px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
            <h2 className={"px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
            <h2 className={"px-12"}>Geeksforgeeks Student Chapter Amla University</h2>
